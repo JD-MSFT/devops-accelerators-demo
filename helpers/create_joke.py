f897cdf8e9a4d4ca0b85d8d7d8d3fe06b6f5e0d1
JD Grillo <jdgrillo@microsoft.com>
2023-04-28 10:41:24 -0700

    fix again

--- helpers/create_joke.py
+++ helpers/create_joke.py
@@ -4,5 +4,4 @@ from helpers.joke_dict import joke_dict
 def create_joke(name, title):
     if title in joke_dict:
         return joke_dict[title] + " " + name
-    else:
-        return joke_dict["Generic"] + " " + name
+    return joke_dict["Generic"] + " " + name
